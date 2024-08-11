70d904e79bf014a046f3199c17f486e21e711020
Maxim Kucherov <maxima-net@bk.ru>
2024-08-11 23:38:31 +0300

    Uncomment metadata

--- scripts/metadata.py
+++ scripts/metadata.py
@@ -1,12 +1,12 @@
 base_template = {
-    # 'version': '2.0.0',
+    'version': '2.0.0',
     'interfaces': ['TZIP-016'], 
     'license': {'name': 'MIT'},
-    # 'homepage': 'https://github.com/tezos-checker/ctez', 
+    'homepage': 'https://github.com/tezos-checker/ctez', 
 }
 
 ctez_fa12_metadata = base_template | {
-    # 'name': 'Ctez FA12',
+    'name': 'Ctez FA12',
     'interfaces': [
         'TZIP-007',
         'TZIP-016'
@@ -14,5 +14,5 @@ ctez_fa12_metadata = base_template | {
 }
 
 ctez_metadata = base_template | {
-    # 'name': 'Ctez',
+    'name': 'Ctez',
 }
